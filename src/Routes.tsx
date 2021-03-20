d41667b63032d226695a45661e6b14b87e774f96
Samit Koyom <samitkoyom@gmail.com>
2021-03-20 22:20:27 +0700

    page complete

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,9 @@
 import {Route, Switch} from 'react-router-dom'
 import FrontLayout from './components/layouts/FrontLayout'
+import About from './pages/about/About'
 import Home from './pages/home/Home'
+import Project from './pages/project/Project'
+import Team from './pages/team/Team'
 
 const AppRoute = ({component: Component, layout: Layout, ...rest}: any) => (
    <Route {...rest} render={ props => (
@@ -12,6 +15,9 @@ const Routes = () => {
     return (
         <Switch>
             <AppRoute path="/" exact={true} layout={FrontLayout} component={Home} />
+            <AppRoute path="/about" layout={FrontLayout} component={About} />
+            <AppRoute path="/teams" layout={FrontLayout} component={Team} />
+            <AppRoute path="/projects" layout={FrontLayout} component={Project} />
         </Switch>
     )
 }
